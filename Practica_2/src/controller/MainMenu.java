26c1bb67f67750d758877ab575afcf1ab8d4cf78
Azogue99 <arna1af@gmail.com>
2024-10-28 15:07:10 +0100

    Quick fix 2
    Making sure name references are correct.

--- Practica_2/src/controller/MainMenu.java
+++ Practica_2/src/controller/MainMenu.java
@@ -11,28 +11,28 @@ import javafx.scene.Node;
 public class MainMenu {
 
     @FXML
-    private void carregarExercici1(ActionEvent event) {
-        carregarExercici("/view/Exercici_02.fxml", "Practica 2 - Exercici 1", event);
-    }
-
-    @FXML
     private void carregarExercici2(ActionEvent event) {
-        carregarExercici("/view/Exercici_03.fxml", "Practica 2 - Exercici 2", event);
+        carregarExercici("/view/Exercici_02.fxml", "Practica 2 - Exercici 2", event);
     }
 
     @FXML
     private void carregarExercici3(ActionEvent event) {
-        carregarExercici("/view/Exercici_04.fxml", "Practica 2 - Exercici 3", event);
+        carregarExercici("/view/Exercici_03.fxml", "Practica 2 - Exercici 3", event);
     }
-    
+
     @FXML
     private void carregarExercici4(ActionEvent event) {
-        carregarExercici("/view/Exercici_05.fxml", "Practica 2 - Exercici 4", event);
+        carregarExercici("/view/Exercici_04.fxml", "Practica 2 - Exercici 4", event);
     }
     
     @FXML
     private void carregarExercici5(ActionEvent event) {
-        carregarExercici("/view/Exercici_06.fxml", "Practica 2 - Exercici 5", event);
+        carregarExercici("/view/Exercici_05.fxml", "Practica 2 - Exercici 5", event);
+    }
+    
+    @FXML
+    private void carregarExercici6(ActionEvent event) {
+        carregarExercici("/view/Exercici_06.fxml", "Practica 2 - Exercici 6", event);
     }
 
     private void carregarExercici(String fxmlPath, String title, ActionEvent event) {
@@ -52,7 +52,7 @@ public class MainMenu {
         try {
             Parent root = FXMLLoader.load(MainMenu.class.getResource("/view/MainMenu.fxml"));
             stage.setScene(new Scene(root));
-            stage.setTitle("Practica 2 - Main Menu");
+            stage.setTitle("Practica 2");
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
